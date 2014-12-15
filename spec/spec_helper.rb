210015426f6ad6fd37bda49b746f4ab7144c3754
David Elner <david@streeteasy.com>
2014-12-15 15:45:30 -0500

    Added: pry to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
 require 'rubygems'
+require 'pry'
 require 'spec/autorun'
 
 require 'typesetter'
